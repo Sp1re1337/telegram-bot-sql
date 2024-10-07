8f6a08a3fbbf765f0cbc7d7507db37ca3c4e6886
Sp1re1337 <yuradtx@gmail.com>
2024-10-07 06:58:52 +0300

    bot.go

--- tg_bot-sqL/bot/bot.go
+++ tg_bot-sqL/bot/bot.go
@@ -9,7 +9,7 @@ import (
 
 // Створюємо нового бота
 func StartBot() error {
-	bot, err := telego.NewBot("7639067110:AAGNdt2TGgoedxh9_0I8LlV0VBYZcvYyQlk")
+	bot, err := telego.NewBot("token")
 	if err != nil {
 		return err
 	}
